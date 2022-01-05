50cf03bc817a1d53fd85c7b5274e58ace47da0a9
Catalina Arriagada <catalina.arriagada.f@mail.pucv.cl>
2022-01-05 08:35:54 -0300

    Primer commit

---
+++ prueba_plotly.py
@@ -0,0 +1,30 @@
+import json
+from urllib.request import urlopen
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.express as px
+
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+                 dtype={"fips": str})
+
+fig = px.choropleth_mapbox(df, geojson=counties, locations='fips', color='unemp',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12),
+                           mapbox_style="carto-positron",
+                           zoom=3, center={"lat": 37.0902, "lon": -95.7129},
+                           opacity=0.5,
+                           labels={'unemp': 'unemployment rate'}
+                           )
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+
+app.run_server(debug=True)  # Turn off reloader if inside Jupyter
